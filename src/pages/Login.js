0758c03ead1ebc078f4a8a6d306120f08875dea0
junmyung <wnsaud9322@naver.com>
2021-12-06 22:20:55 +0900

    1206 commit3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {Text, Input, Grid, Button} from "../elements";
+
 
 const Login = (props) => {
 
+    const [id, setId] = React.useState("");
+    const [pwd, setPwd] = React.useState("");
+
     return (
-        <React.Fragment>
-            로그인이다!!
-        </React.Fragment>
+        <>
+            <Text size="40px" bold>
+                로그인
+            </Text>
+            <Input placeholder="아이디"></Input>
+            <Input placeholder="비밀번호(영문,숫자,특수 문자 포함 8자 이상)"></Input>
+            <Button>로그인</Button>
+        </>
     );
 };
 
