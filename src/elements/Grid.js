217b1898bf8584e4ae3f2bb93867bde888028448
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-12-06 22:14:27 +0900

    elements update

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -1,12 +1,77 @@
 import React from "react";
+import styled from 'styled-components';
 
 const Grid = (props) => {
 
-    return (
-        <React.Fragment>
+    const {position,children,flex,width,padding,margin,_onClick,contentWrap,signWrap} = props;
+    const styles = {
+        flex:flex,
+        position:position,
+        width:width,
+        padding:padding,
+        margin:margin,
+    }
 
-        </React.Fragment>
+
+    if(contentWrap){
+        return (
+            <ContentWrap  {...styles}  onClick={_onClick}>
+                {children}
+            </ContentWrap>
+        );
+    }
+
+    if(signWrap){
+        return (
+            <SignWrap {...styles}  onClick={_onClick}>
+                {children}
+            </SignWrap> 
+        );
+    }
+
+    return (
+        <GridBox {...styles}  onClick={_onClick}>
+                {children}
+        </GridBox>
     );
 };
+Grid.defaultProps ={
+    position:false,
+    children:null,
+    flex:false,
+    width:"100%",
+    padding:false,
+    margin:false,
+    _onClick:()=>{}
+}
+
+const GridBox = styled.div`
+    position : ${(props)=> props.position ? `${props.position}`:""};
+    width:${(props) =>props.width};   
+    height:100%;
+    box-sizing:border-box;
+    padding: ${(props)=> props.padding ? `${props.padding}` : ""};
+    margin:${(props)=> props.margin ? `${props.margin}`:""};
+    display:${(props)=> props.is_flex ? `${props.is_flex}`:""};
+    
+`;
+
+const ContentWrap = styled.div`
+    position:relative;
+    width:1080px;
+    margin:0 auto;
+    border:1px solid #eee;  // 임시로 지정
+    text-align:center;
+
+`;
+const SignWrap = styled.div`
+    position:relative;
+    width:400px;
+    margin:0 auto;
+    border:1px solid #eee;  // 임시로 지정
+    text-align:center;
+    padding: 100px 20px; 
+
+`;
 
 export default Grid;
