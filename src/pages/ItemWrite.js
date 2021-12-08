70c72ca3d65b6b6846c6bfc0b22b3b8ed9fa0f08
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-12-09 01:59:07 +0900

    axios

--- src/pages/ItemWrite.js
+++ src/pages/ItemWrite.js
@@ -1,12 +1,64 @@
 import React from "react";
 import {Input, Text, Button,Grid} from '../elements/index';
 import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
+import { actionCreators as itemActions } from "../redux/modules/item";
 
 const PostWrite = (props) => {
+    const dispatch = useDispatch();
     const history = useHistory();
     const fileInput = React.useRef();
+    const [title, setTitles] = React.useState("");
+    const [price, setPrices] = React.useState("");
+    const [targetPrice, settargetPrice] = React.useState("");
+    const [textarea, setTexts] = React.useState("");
+
+    const change_title = (e) => {
+        setTitles(e.target.value);
+    }
+    const change_price = (e) => {
+        setPrices(e.target.value);
+    }
+    const change_goal = (e) => {
+        settargetPrice(e.target.value);
+    }
+    const change_text = (e) => {
+        setTexts(e.target.value);
+    }
+    
+    /////////
+    const selectFile = (e) => { 
+        console.log(e);
+        console.log(e.target.files);   
+        console.log(e.target.files[0]);
+    
+        console.log(fileInput.current.files[0]);
+    
+        const reader = new FileReader();
+        const file = fileInput.current.files[0];
+    
+        reader.readAsDataURL(file);
+    
+        console.log(reader.result);
+      };
+
+
+
+    //////
+
+        const addItem = () => {
+            console.log("게시글 작성버튼 누름!")
+            
+            dispatch(itemActions.addItemNJ(title, price, targetPrice, textarea, fileInput.current.files[0]));
+            
+        }
+    
+   
+    
 
     return (
+
+        
         <>
           <Grid contentWrap>
             <Grid _onClick={()=>{history.push("/");}}>
@@ -16,19 +68,22 @@ const PostWrite = (props) => {
             </Grid>
 
             
-            <Text  align="left" size="20px"bold >
+            <Text  align="left" size="20px" bold >
                 nickname님!<br/>
                 새로운 프로젝트를 소개해보세요.
             </Text>
 
             <Grid>
-                <Input InputTitle label="프로젝트 제목*" placeholder="프로젝트 제목을 입력하세요." margin="40px 0 0 0"></Input>
-                <Input InputTitle label="상품 금액*" placeholder="하나의 상품 금액을 입력하세요."  margin="40px 0 0 0"></Input>
-                <Input InputTitle label="목표 금액*" placeholder="상품금액 10배이상을 지정해주세요." margin="40px 0 0 0"></Input>
-                <Input ref={fileInput} type="file" InputTitle label="상품 이미지 등록*" placeholder="이미지 파일" margin="40px 0 0 0"></Input>
-                <Input textarea label="프로젝트 상세 내용*" placeholder="프로젝트 상세 내용을 입력하세요." margin="40px 0 15px 0"></Input>
+                <Input value={title} _onChange={change_title} InputTitle label="프로젝트 제목*" placeholder="프로젝트 제목을 입력하세요." margin="40px 0 0 0"></Input>
+                <Input value={price} _onChange={change_price} InputTitle label="상품 금액*" placeholder="하나의 상품 금액을 입력하세요."  margin="40px 0 0 0"></Input>
+                <Input value={targetPrice} _onChange={change_goal} InputTitle label="목표 금액*" placeholder="상품금액 10배이상을 지정해주세요." margin="40px 0 0 0"></Input>
+                <input ref={fileInput} type="file" onChange={selectFile}/>
+                <Input value={textarea} _onChange={change_text} textarea label="프로젝트 상세 내용*" placeholder="프로젝트 상세 내용을 입력하세요." margin="40px 0 15px 0"></Input>
                 
-                <Button text="등록하기" _onClick={()=>{history.push('/')}}></Button>
+                <Button text="등록하기" _onClick={()=>{
+                    addItem();
+                    history.push('/');
+                    }}></Button>
             </Grid>
             
           </Grid>
