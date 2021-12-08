729e36180a126be6c0e5fa2a0d3199695a2f8120
ireneeming <63698668+ireneeming@users.noreply.github.com>
2021-12-08 15:20:20 +0900

    axios failed

--- src/pages/ItemList.js
+++ src/pages/ItemList.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {Grid,Image,Text} from '../elements/index';
 import { useSelector,  } from "react-redux";
 import {history} from '../redux/configureStore';
-
+import api from '../api/items';
 
 import Item from "../components/Item";
 import Rank from "../components/Rank";
 
 const ItemList = (props) => {
     const item_list = useSelector((state)=> state.item.result)
+    const [itemIdx,setitemIdx]= useState(0);
+    const [result,setResult] = useState([{
+        idx:"",
+        title: "",
+        images: "",
+        thumbnail:"",
+        price:"",
+        targetPrice: "",
+        content:"",
+        totalPrice:""
+      }]);
+
+    useEffect(()=>{
+        const fetchItem = async () => {
+            try{
+                const response = await api.get('/result');
+                const _inputData = await response.data.map((rowData)=>(
+                    setitemIdx(itemIdx+1),
+                    {
+                        idx: rowData.idx,
+                        title: rowData.title,
+                        images: rowData.image,
+                        thumbnail:rowData.thumbnail,
+                        price:rowData.price,
+                        targetPrice: rowData.targetPrice,
+                        content:rowData.content,
+                        totalPrice:rowData.totalPrice
+                    }
+                ))
+
+                setResult(result.concat(_inputData));
+            }catch(e){
+                console.error(e.message);
+            }
+        }
+        fetchItem();
+    },[]);
 
 
 
@@ -24,8 +61,8 @@ const ItemList = (props) => {
                    
                   <Grid  flex="flex;flex-wrap:wrap;justify-content:space-between;">
                    {
-                       item_list.map((p,idx)=>{
-                        return  <Item key={p.id} {...p}  _onClick={()=>{history.push('/item/'+idx)}}/>
+                       item_list.map(rowData =>{
+                        return  <Item key={rowData.idx} {...rowData}  _onClick={()=>{history.push(`/item/${rowData.idx}`)}}/>
                         
                        })
                    }
