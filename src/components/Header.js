088dda07379348db7db2e87197796fad0dad402b
junmyung <wnsaud9322@naver.com>
2021-12-11 14:50:40 +0900

    header commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,17 +13,24 @@ const Header = (props) => {
   // const user = useSelector((state)=>state.user.nickname);
   const local_token = localStorage.getItem('token');
 
+  const item = useSelector((state) => state.item.edit_item.contents);
+  const is_edit = item.id? true: false;
+
 
   
   if(local_token) {
     return(
       <Grid contentWrap flex="flex;justify-content:space-between;align-items:center;">
-        <Grid  width="auto" _onClick={()=>{history.push('/')}}>
+        <Grid  width="auto" _onClick={()=>{history.push('/')
+                                            window.location.reload("/")}}>
             <Text align="left" size="30px">Eco Funding</Text>
         </Grid>
         <Grid  width="auto">
             <Button width="200px" margin="0 10px" text="내정보"></Button>
+            {is_edit?<></>:
             <Button width="200px" margin="0 10px" text="펀딩 등록하기" _onClick={()=>{history.push('/write/a');}}></Button>
+            }
+            
             <Button width="200px" margin="0 10px" text="로그아웃" _onClick={()=>{
               dispatch(userActions.logOut());
               window.location.reload();
